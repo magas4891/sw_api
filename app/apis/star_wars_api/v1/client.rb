5c11d2000cb4e87fa65ba5e92445068066ae4bf2
magas4891 <magasishe@gmail.com>
2020-06-03 13:24:32 +0300

    all requests - done

--- app/apis/star_wars_api/v1/client.rb
+++ app/apis/star_wars_api/v1/client.rb
@@ -1,24 +1,105 @@
 module StarWarsApi
   module V1
     class Client
-      API_ENDPOINT = 'https://swapi.dev/api/'.freeze
+      BASE_URL = 'https://swapi.dev/api'.freeze
+
+      def root_request
+        request(
+            'get',
+            '/'
+        )
+      end
 
       def get_peoples
-        
+        request(
+          'get',
+          '/people/'
+        )
+      end
+
+      def show_people(id)
+        request(
+            'get',
+            "/people/#{id}/"
+        )
+      end
+
+      def get_films
+        request(
+            'get',
+            '/films/'
+        )
+      end
+
+      def show_film(id)
+        request(
+            'get',
+            "/films/#{id}/"
+        )
+      end
+
+      def get_starships
+        request(
+            'get',
+            '/starships/'
+        )
+      end
+
+      def show_starship(id)
+        request(
+            'get',
+            "/starships/#{id}/"
+        )
+      end
+
+      def get_vehicles
+        request(
+            'get',
+            '/vehicles/'
+        )
+      end
+
+      def show_vehicle(id)
+        request(
+            'get',
+            "/vehicles/#{id}/"
+        )
+      end
+
+      def get_species
+        request(
+            'get',
+            '/species/'
+        )
+      end
+
+      def show_species(id)
+        request(
+            'get',
+            "/species/#{id}/"
+        )
+      end
+
+      def get_planets
+        request(
+            'get',
+            '/planets/'
+        )
+      end
+
+      def show_planet(id)
+        request(
+            'get',
+            "/planets/#{id}/"
+        )
       end
 
       private
 
-      def client
-        @_client ||= Faraday.new(API_ENDPOINT) do |client|
-          client.request :url_encoded
-          client.adapter Faraday.default_adapter
-        end
-      end
-
-      def request(http_method:, endpoint:, params: {})
-        response = client.public_send(http_method, API_ENDPOINT + endpoint, params)
-        Oj.load(response.body)
+      def request(http_method, url)
+        connection = Faraday.new(BASE_URL + url)
+        response = connection.send(http_method)
+        # Oj.load(response.body)
       end
     end
   end
